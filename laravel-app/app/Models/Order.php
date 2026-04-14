72ad75e039d2659aa83451813336aca7ec13b30b
OferTrabajo <ofertrabajo.contacto@gmail.com>
2026-04-14 10:54:10 -0500

    add order and order item models with own,s relationships

---
+++ laravel-app/app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'patient_name',
+        'status',
+        'validation_reason',
+    ];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
